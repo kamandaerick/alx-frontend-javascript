d953d0756eedb62e251e0b4464454d78ecc48a1d
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-05 10:03:50 +0300

    Use rest parameters

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,8 @@
+/* eslint-disable no-unused-vars */
 export default function returnHowManyArguments(...args) {
   let totalArgs = 0;
-  for (let arg of args) {
+  for (const arg of args) {
     totalArgs += 1;
   }
   return totalArgs;
 }
-
-console.log(returnHowManyArguments(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
