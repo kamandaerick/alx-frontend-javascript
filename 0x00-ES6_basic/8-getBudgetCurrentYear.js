7357b6966fcf89bb37247db3a2660ae15455c0f7
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-05 11:37:33 +0300

    Using computed property names for objects

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,10 +1,9 @@
-/* eslint-disable no-unused-vars */
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
 
-function getBudgetForCurrentYear(income, gdp, capita) {
+export default function getBudgetForCurrentYear(income, gdp, capita) {
   return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
