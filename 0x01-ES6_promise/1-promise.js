884855fc8d3cf27db46a265e4b53d878e16c19c3
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-07 23:32:39 +0300

    Task 1 solution

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(bool) {
         status: 200,
         body: 'Success',});
     } else {
-      reject throw new Error('The fake API is not working currently');
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
