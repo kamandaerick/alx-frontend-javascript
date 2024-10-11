0148306bbe09b150e0edaf5649280d0f2279cd15
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 00:20:06 +0300

    Filter and map combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);      
-      return {
-        ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A',
-      };
-    });
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: gradeObj ? gradeObj.grade : 'N/A',
+    };
+  });
 }
