806951d57f5de50258bb2a4a929af27753f4a285
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 00:16:02 +0300

    Filter and map combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,5 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
+  return students.filter((student) => student.location === city).map((student) => {
       const gradeObj = newGrades.find((grade) => grade.studentId === student.id);      
       return {
         ...student,
