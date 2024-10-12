a86171e310d581e415eef3396a3a0d6844c8e272
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 15:17:53 +0300

    Revised

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (let [k, v] of map) {
+  for (const [k, v] of map) {
     if (v === 1) {
       map.set(k, 100);
     }
