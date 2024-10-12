1d68a65a6de55c953d161e5629bbe1317bdf1bfb
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 12:27:54 +0300

    Typed Arrays ciorrected to return a dataview

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const dataView = new DataView(buffer);
-  if (position < 0 || position >= dataView.length) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   } else {
     dataView.setInt8(position, value);
