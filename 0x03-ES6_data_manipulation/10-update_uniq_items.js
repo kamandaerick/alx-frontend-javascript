4177f2de59e7c46b981c1e74eee0f8b956b56638
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 15:12:38 +0300

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqItems(map) {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process');
+  }
   for (let [k, v] of map) {
     if (v === 1) {
       map.set(k, 100);
