12360b433ca16a4f3906736e73ab3e9df5681029
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-12 13:48:17 +0300

    Clean Set: export function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
-  const result = [...set].filter((value) => value.startsWith(startString))
-  .map((value) => value.slice(startString.length)).join('-');
-  return result;
+  const result = [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+  return result || '';
 }
